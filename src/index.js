86d666072f2c3b3afb93447eadd495563faa0409
Val Geyvandov <vgeyvandov@madmobile.com>
2021-08-28 18:49:12 -0400

    Building out the bar graph, pulling in immutability-helper for more performant array updates, setting up styles, almost time to refactor App component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import GlobalStyles from './globalStyles';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyles />
     <App />
   </React.StrictMode>,
   document.getElementById('root')

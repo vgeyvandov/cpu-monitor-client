1be2cccb219c0b19146f13d39ad0b711424d0fa4
Val Geyvandov <vgeyvandov@madmobile.com>
2021-08-28 14:47:14 -0400

    Initial setup of the cpu monitor client, removing some template code, testing out cpu-average fetch, setting up estlint+prettier

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
 
 function App() {
+  useEffect(async () => {
+    const response = await fetch('http://localhost:3001/cpu-average');
+    const average = await response.json();
+    console.log('average', average);
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
